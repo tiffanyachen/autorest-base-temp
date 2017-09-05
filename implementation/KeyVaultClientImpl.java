7798326cfaadd2202cbfd7853591e90c7c3eb98d
tiffanyachen <tifchen@microsoft.com>
2017-09-05 16:56:49 -0700

    starting mods

---
+++ implementation/KeyVaultClientImpl.java
@@ -0,0 +1,31 @@
+import .models.StorageListResult;
+import com.google.common.base.Joiner;
+import com.google.common.reflect.TypeToken;
+import com.microsoft.rest.ServiceCallback;
+import com.microsoft.rest.ServiceFuture;
+import com.microsoft.rest.ServiceResponse;
+import com.microsoft.rest.Validator;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import okhttp3.ResponseBody;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Headers;
+import retrofit2.http.HTTP;
+import retrofit2.http.PATCH;
+import retrofit2.http.Path;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Query;
+import retrofit2.Response;
+import rx.functions.Func1;
+import rx.Observable;
+
+/**
+ * Initializes a new instance of the KeyVaultClientBase class.
+ */
+public class KeyVaultClientImpl extends KeyVaultClientBaseImpl implements KeyVaultClient {
+    
+}
